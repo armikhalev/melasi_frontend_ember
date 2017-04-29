528c15d0a905356bc724d3e426a0a28538ac3876
armikhalev <armikhalev@gmail.com>
2017-04-28 20:04:24 -0400

    Drying up koyla code. Not finished yet.

--- app/controllers/koyla.js
+++ app/controllers/koyla.js
@@ -4,25 +4,41 @@ import { ensurePromise } from '../utils/ensurePromise';
 export default Ember.Controller.extend({
 
   langTranslateFrom: 'English',
+  langTranslateTo: 'Mela',
   english:true,
+  settings: Ember.computed('english', function(){
+      let settings = {};
+
+      // Check which is the current language and change settings accordingly
+      if (this.get('english') === true) {
+        settings.letters = 'firstLetters';
+        settings.modelQuery = 'word';
+      }
+      else
+      {
+        settings.letters = 'gesewlaLiki';
+        settings.modelQuery = 'la';
+      }
+     return settings;
+  }),
   firstLetters:[],
   gesewlaLiki:[],
 
   actions: {
     filterByWord(param) {
-      // Check the first letter of the input param and get the data from serever. Don't reload data from server anymore.
-      if (param !== '' && param.length < 2) {
+        let _letters = this.get('settings').letters;
+        let _modelQuery = this.get('settings').modelQuery;
+
         // Check if the param letter was previously used if yes then don't call backend, peek data from local ember store
-        if (this.get('firstLetters').lastIndexOf(param) === -1) {
-                     
-          this.get('firstLetters').pushObject(param);
-          return this.store.query('word',{ letter:param });
+        if  ( (param !== "") && (this.get(_letters).lastIndexOf(param) === -1) ) {             
+          this.get(_letters).pushObject(param);
+          return this.store.query(_modelQuery,{ letter:param });
         }
-        else {
-          let search = this.store.peekAll('word');
+        else if (this.get(_letters).lastIndexOf(param) !== -1) {
+          let search = this.store.peekAll(_modelQuery);
           
           let filtered = search.filter(function(i) {
-            return i.get("word").toLowerCase().indexOf(param.toLowerCase()) !== -1;
+            return i.get(_modelQuery).toLowerCase().indexOf(param.toLowerCase()) !== -1;
           });
           
           if (search.get('length') >= 1000)
@@ -32,78 +48,13 @@ export default Ember.Controller.extend({
           // store.peekAll() returns Ember.enumerable class object, not a promise. Make it to be promise.
           return ensurePromise(filtered);
         } 
-      }
-      // When input param is more than one letter, peeek all the words from Ember store with first letter as in input param.
-      // Check second and all the following letters to be equal to those strings that in Ember store.
-      else if (param !== '' && param.length >= 2) {
-
-          let search = this.store.peekAll('word');
-          
-          let filtered = search.filter(function(i) {
-            return i.get("word").toLowerCase().indexOf(param.toLowerCase()) !== -1;
-          });
-          
-          if (search.get('length') >= 1000)
-          {
-            this.store.unloadAll();
-          }
-          // store.peekAll() returns Ember.enumerable class object, not a promise. Make it to be promise.
-          return ensurePromise(filtered);
-      } 
-      else {
-        return this.get('store').query('word',{letter:"a"});
-      }
-    },
-    /***********************************
-     * Mela filter, TEMP, must be DRY *
-     ********************************/
-    filterByLa(param) {
-      // Check the first letter of the input param and get the data from serever. Don't reload data from server anymore.
-      if (param !== '' && param.length < 2) {
-        // Check if the param letter was previously used if yes then don't call backend, peek data from local ember store
-        if (this.get('gesewlaLiki').lastIndexOf(param) === -1) {
-                     
-          this.get('gesewlaLiki').pushObject(param);
-          return this.store.query('la',{ letter:param });
-        }
         else {
-          let search = this.store.peekAll('la');
-          
-          let filtered = search.filter(function(i) {
-            return i.get("la").toLowerCase().indexOf(param.toLowerCase()) !== -1;
-          });
-          
-          if (search.get('length') >= 1000)
-          {
-            this.store.unloadAll();
-          }
-          // store.peekAll() returns Ember.enumerable class object, not a promise. Make it to be promise.
-          return ensurePromise(filtered);
-        } 
-      }
-      // When input param is more than one letter, peeek all the words from Ember store with first letter as in input param.
-      // Check second and all the following letters to be equal to those strings that in Ember store.
-      else if (param !== '' && param.length >= 2) {
-
-          let search = this.store.peekAll('la');
-          
-          let filtered = search.filter(function(i) {
-            return i.get("la").toLowerCase().indexOf(param.toLowerCase()) !== -1;
-          });
-          
-          if (search.get('length') >= 1000)
-          {
-            this.store.unloadAll();
-          }
-          // store.peekAll() returns Ember.enumerable class object, not a promise. Make it to be promise.
-          return ensurePromise(filtered);
-      } 
-      else {
-        return this.get('store').query('word',{letter:"a"});
-      }
+          return this.get('store').query('word',{letter:"a"});
+        }
     },
     changeLanguage_onClick () {
       this.setProperties({langTranslateFrom: this.langTranslateFrom === 'English' ? 'Mela' : 'English'});
+      this.setProperties({langTranslateTo: this.langTranslateTo === 'Mela' ? 'English' : 'Mela'});
       this.setProperties({english: this.langTranslateFrom === 'English' ? true : false});
     }
   }
