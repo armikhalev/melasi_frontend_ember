e4b4e5e884f0978f86b2ea7d7c9d383c966c0dc6
armikhalev <armikhalev@gmail.com>
2017-05-29 17:30:52 -0400

    Adding intro page.

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('koyla', {path: '/koyla'});
   this.route('latay', {path: '/latay'});
+  this.route('intro', {path: '/'});
 });
 
 export default Router;
