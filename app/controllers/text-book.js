6a74aa238e47646708f7138629c74d0621c64ea8
armikhalev <armikhalev@gmail.com>
2017-06-27 21:34:33 -0400

    Text-book: fixes sorting of the cards by category.

--- app/controllers/text-book.js
+++ app/controllers/text-book.js
@@ -1,6 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    sorting: ['category:asc'],
-    grammarCard: Ember.computed.sort('model', 'sorting')
+
+    grammarCards: Ember.computed('model', function(){
+        let _cardCategories = {};
+        
+        this.get('model').forEach((card) => { 
+            let cat = Ember.get(card, 'category');
+            if(!_cardCategories[cat]) {
+                _cardCategories[cat] = [];
+            }
+            _cardCategories[cat].push(card);
+        });
+
+        const orderedCategories = {};
+        Object.keys(_cardCategories).sort().forEach(function(key) {
+            orderedCategories[key] = _cardCategories[key];
+        });
+
+        return orderedCategories;
+    })
 });
